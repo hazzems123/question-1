0777449ec2d82679baa4ea67e62e0a50fb4963e0
hazzems123 <hazzems@hotmail.com>
2021-06-09 00:33:35 +0200

    Add files via upload

---
+++ first question quiz.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[70]:
+
+
+import numpy as np
+
+
+# In[80]:
+
+
+x = list(np.random.randint(low = 2000,high=3200,size=50))
+
+
+# In[81]:
+
+
+print (x)
+
+
+# In[87]:
+
+
+f = []
+
+
+# In[90]:
+
+
+for i in x :
+    if i % 7 ==0 and i % 5 != 0 :
+        f.append(i)
+    else:
+        x.remove(i)
+
+
+# In[91]:
+
+
+print (f)
+
+
+# In[ ]:
+
+
+
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+  
+
+
+# In[ ]:
+
+
+
+
+
+# In[66]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
